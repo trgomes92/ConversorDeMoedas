ef9929b314bdbbb6a4b7c2aaf72e803820104ba3
Juan Gomes <juangsantanna@gmail.com>
2022-01-27 12:10:27 -0300

    Header feitos, juntamente de alguns itens do dropdown de seleção de moedas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header/header.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ContainerComponent } from './container/container.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
